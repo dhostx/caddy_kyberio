fc9a2d45795da6514931a4d42bbc9ecd574d3b39
Damian Hopa <damian@hopa.pl>
2025-05-20 18:11:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Kzberio module for Caddy
+
+This package contains a DNS provider module for [Caddy](https://github.com/caddyserver/caddy). It can be used to manage DNS records with Kyberio's Domainrobot accounts.
+
+## Caddy module name
+
+```
+dns.providers.kyberio
+```
+
+## Config examples
+
+To use this module for the ACME DNS challenge, [configure the ACME issuer in your Caddy JSON](https://caddyserver.com/docs/json/apps/tls/automation/policies/issuer/acme/) like so:
+
+```json
+{
+  "module": "acme",
+  "challenges": {
+    "dns": {
+      "provider": {
+        "name": "kyberio",
+        "api_token": "THE_ZONE_KEY"
+      }
+    }
+  }
+}
+```
+
+or with the Caddyfile:
+
+```
+your.domain.com {
+  respond "Hello World"	# replace with whatever config you need...
+  tls {
+    dns kyberio {env.THE_ZONE_KEY}
+  }
+}
+```
+
+You can replace `{env.THE_ZONE_KEY}` with the actual auth token if you prefer to put it directly in your config instead of an environment variable.
+
+## Authenticating
+
+For each zone, a zone key is required. To generate the key, access your DNS entry in the Domainrobot, click on "Passwort für dynamic DNS generieren" and save the zone.
---
+++ kyberio.go
@@ -0,0 +1,70 @@
+package caddy_kyberio
+
+import (
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/dhostx/libdns_kyberio"
+)
+
+// Provider wraps the provider implementation as a Caddy module.
+type Provider struct{ *sdns.Provider }
+
+func init() {
+	caddy.RegisterModule(Provider{})
+}
+
+// CaddyModule returns the Caddy module information.
+func (Provider) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "dns.providers.kyberio",
+		New: func() caddy.Module { return &Provider{new(sdns.Provider)} },
+	}
+}
+
+// Before using the provider config, resolve placeholders in the API token.
+// Implements caddy.Provisioner.
+func (p *Provider) Provision(ctx caddy.Context) error {
+	repl := caddy.NewReplacer()
+	p.Provider.APIToken = repl.ReplaceAll(p.Provider.APIToken, "")
+	return nil
+}
+
+// UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
+//
+//	hetzner [<api_token>] {
+//	    api_token <api_token>
+//	}
+func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		if d.NextArg() {
+			p.Provider.APIToken = d.Val()
+		}
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "api_token":
+				if p.Provider.APIToken != "" {
+					return d.Err("API token already set")
+				}
+				p.Provider.APIToken = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			default:
+				return d.Errf("unrecognized subdirective '%s'", d.Val())
+			}
+		}
+	}
+	if p.Provider.APIToken == "" {
+		return d.Err("missing API token")
+	}
+	return nil
+}
+
+// Interface guards
+var (
+	_ caddyfile.Unmarshaler = (*Provider)(nil)
+	_ caddy.Provisioner     = (*Provider)(nil)
+)
